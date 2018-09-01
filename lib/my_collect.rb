e4bb9f55df5737b1a3dd9af87befab4617f96c82
Chribtoper <xcstolfax@gmail.com>
2018-09-01 20:44:03 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,10 @@ def my_collect(arg)
   i = 0
   arr = []
   while i < arg.length
-arr << yield (arg[i].split(" ").first)
+arr << yield (arg[i])
 i += 1
 end
 arr
 end
+
+my_collect(arg[i].split(" ").first
